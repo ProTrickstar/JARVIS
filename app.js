7477117791fedc3532520c2e272555b919a4ce37
Swastik Singh <71075971+ProTrickstar@users.noreply.github.com>
2021-06-05 12:26:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const microphoneButton = document.getElementById("microphoneButton");
 
 microphoneButton.addEventListener("click", function () {
 	window.open(
-		"http://localhost:5500/mic/",
+		"https://github.com/ProTrickstar/JARVIS/blob/master/mic",
 		"nativeWindow",
 		"width=350,height=300"
 	);
